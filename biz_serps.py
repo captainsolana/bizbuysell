1f6dfa1193dd4b2973dd908ef16ff02ee177d5f1
Lance Johnson <lancejohnson728@gmail.com>
2019-12-23 11:51:48 -0500

    Split up functions

--- biz_serps.py
+++ biz_serps.py
@@ -6,6 +6,7 @@ import httpx
 import json
 import os
 import pdb
+import pickle
 import re
 from tenacity import retry, stop_after_attempt
 
@@ -46,6 +47,63 @@ class Listing():
         return self.custom_name
 
 
+def parse_listings(listing_objs):
+    def parse_financials_div(financials_soup):
+        try:
+            financials_text = financials_soup.text
+            financials_list = financials_text.split("\r\n")[:-1]
+            financials_dict = list_to_dict(financials_list)
+
+            not_included = []
+            for key in financials_dict:
+                if "*" in financials_dict[key]:
+                    not_included.append(key)
+
+            financials_dict["notIncluded"] = not_included
+
+            for key in financials_dict:
+                try:
+                    financials_dict[key] = int(re.sub("[^0-9]", "", financials_dict[key]))
+                except Exception:
+                    continue
+
+            return financials_dict
+        except Exception as e:
+            print(e)
+            pdb.set_trace()
+
+    def parse_details_div(details_soup):
+        try:
+            details_tag_list = details_soup.contents
+            details_str = " ".join([str(element) for element in details_tag_list])
+            details_list = details_str.split("<dt>")[1:]
+            strs_to_tags = [BeautifulSoup(detail, "html.parser") for detail in details_list]
+            details_text = [tag.text for tag in strs_to_tags]
+            details_dict = list_to_dict(details_text)
+
+            return details_dict
+        except Exception as e:
+            print(e)
+            pdb.set_trace()
+
+    # Parse available listing fields into a dict
+    for listing_obj in listing_objs:
+        print(f"Starting {listing_obj.url}")
+        soup = BeautifulSoup(listing_obj.response_text, "html.parser")
+
+        # Price details
+        financials_soup = soup.find("div", {"class": "row-fluid b-margin financials clearfix"})
+        if financials_soup:
+            financials_dict = parse_financials_div(financials_soup)
+            listing_obj.financials = financials_dict
+
+        # Listing Details
+        details_soup = soup.find("dl", {"class": "listingProfile_details"})
+        if details_soup:
+            details_dict = parse_financials_div(details_soup)
+            listing_obj.details = details_dict
+
+
 async def fetch_listings(*, listing_objs, con_limit):
     async def fetch_obj_with_url(obj):
         SCRAPER_API_KEY = os.environ.get('SCRAPER_API_KEY', '')
@@ -67,55 +125,9 @@ async def fetch_listings(*, listing_objs, con_limit):
                 obj.response_text = "Soup test failed"
                 raise ValueError(f"Soup test failed for {obj.custom_name}")
 
-    def parse_financials_div(financials_soup):
-        financials_text = financials_soup.text
-        financials_list = financials_text.split("\r\n")[:-1]
-        financials_dict = list_to_dict(financials_list)
-
-        not_included = []
-        for key in financials_dict:
-            if "*" in financials_dict[key]:
-                not_included.append(key)
-
-        financials_dict["notIncluded"] = not_included
-
-        for key in financials_dict:
-            try:
-                financials_dict[key] = int(re.sub("[^0-9]", "", financials_dict[key]))
-            except Exception:
-                continue
-
-        return financials_dict
-
-    def parse_details_div(details_soup):
-        details_tag_list = details_soup.contents
-        details_str = " ".join([str(element) for element in details_tag_list])
-        details_list = details_str.split("<dt>")[1:]
-        strs_to_tags = [BeautifulSoup(detail, "html.parser") for detail in details_list]
-        details_text = [tag.text for tag in strs_to_tags]
-        details_dict = list_to_dict(details_text)
-
-        return details_dict
-
     # Scrape all the listings (save response to an object)
     pool = AioPool(size=con_limit)
     await pool.map(fetch_obj_with_url, listing_objs)
-
-    # Parse available listing fields into a dict
-    for listing_obj in listing_objs:
-        soup = BeautifulSoup(listing_obj.response_text, "html.parser")
-
-        # Price details
-        financials_soup = soup.find("div", {"class": "row-fluid b-margin financials clearfix"})
-        if financials_soup:
-            financials_dict = parse_financials_div(financials_soup)
-            listing_obj.financials = financials_dict
-
-        # Listing Details
-        details_soup = soup.find("dl", {"class": "listingProfile_details"})
-        if details_soup:
-            details_dict = parse_financials_div(details_soup)
-            listing_obj.details = details_dict
 
 
 async def fetch_listing_urls(*, con_limit):
@@ -281,7 +293,7 @@ async def fetch_listing_urls(*, con_limit):
         del categories["name"]
 
     data_objects = generate_serp_objs()
-    data_objects = data_objects[155:160]
+    # data_objects = data_objects[155:160]
 
     print(f"{len(data_objects)} categories to process")
     pool = AioPool(size=con_limit)
@@ -301,15 +313,37 @@ async def fetch_listing_urls(*, con_limit):
     # Get Listing Objs
     listing_objs = []
     for serp_response in serp_response_list:
-        listing_obj_holding = create_listing_obj_from_response(serp_response)
-        if listing_obj_holding:
-            listing_objs.extend(listing_obj_holding)
+        try:
+            listing_obj_holding = create_listing_obj_from_response(serp_response)
+            if listing_obj_holding:
+                listing_objs.extend(listing_obj_holding)
+        except Exception as e:
+            print(e)
+            continue
 
     return listing_objs
 
 
-if __name__ == "__main__":
-    con_limit = 10
+def full_function():
+    con_limit = 25
     listing_objs = asyncio.run(fetch_listing_urls(con_limit=con_limit))
     asyncio.run(fetch_listings(listing_objs=listing_objs, con_limit=con_limit))
-    pdb.set_trace()
+    parse_listings(listing_objs)
+
+
+def fetch_listing_html_write_to_pickle():
+    con_limit = 25
+    listing_objs = asyncio.run(fetch_listing_urls(con_limit=con_limit))
+    asyncio.run(fetch_listings(listing_objs=listing_objs, con_limit=con_limit))
+    with open("/Users/work/Dropbox/Projects/Working Data/bizbuysell/listings20191221.pkl", "wb") as outfile:
+        pickle.dump(listing_objs, outfile)
+
+
+def parse_listings_from_pkl():
+    with open("/Users/work/Dropbox/Projects/Working Data/bizbuysell/listings20191221.pkl", "rb") as infile:
+        listing_objs = pickle.load(infile)
+    parse_listings(listing_objs)
+
+
+if __name__ == "__main__":
+    fetch_listing_html_write_to_pickle()
